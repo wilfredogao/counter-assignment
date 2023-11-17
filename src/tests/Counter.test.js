890906744e217be53f0523486aa466f40fc41d8e
Wil <wilfredo.gao@gmail.com>
2023-11-16 22:21:16 -0500

    Render counter component

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,9 +1,12 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
-})
+  render(<Counter />);
+})  
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
